4e08be023d4b1baf364fe4b653b8bb2427caee44
TDMWalpala <tharindudarshana2019@gmail.com>
2021-11-01 19:41:27 +0530

    Binary search tree implementation c++

---
+++ Tree/BST/BST.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#define SPACE 10
+
+using namespace std;
+
+class TreeNode{
+public:
+    int value;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(){
+        value = 0;
+        left = NULL;
+        right = NULL;
+    }
+
+    TreeNode(int v){
+        value = v;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class BST{
+public:
+    TreeNode *root;
+    BST(){
+        root = NULL;
+    }
+
+    bool isEmpty(){
+        if(root==NULL){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    void insertNode(TreeNode *newNode){
+        if(root==NULL){
+            root = newNode;
+            cout<<"Value inserted as root node"<<endl;
+        }
+        else{
+            TreeNode *temp = root;
+            while(temp!=NULL){
+                if(newNode->value==temp->value){
+                    cout<<"You inserted value is already exist!"<<endl;
+                    return;
+                }else if((newNode->value < temp->value)&&(temp->left==NULL)){
+                    temp->left = newNode;
+                    cout<<"Value inserted"<<endl;
+                    break;
+                }else if(newNode->value < temp->value){
+                    temp = temp->left;
+                }else if((newNode->value > temp->value) && (temp->right==NULL)){
+                    temp->right = newNode;
+                    cout<<"Value inserted"<<endl;
+                    break;
+                }else
+                    temp = temp->right;
+                }
+            }
+        }
+
+    void print(TreeNode *r, int space){
+        if(r==NULL){
+            return;
+        }
+        space += SPACE;
+        print(r->right, space);
+        cout << endl;
+        for(int i=SPACE; i<space;i++){
+            cout<<" ";
+        }
+        cout<<r->value<<"\n";
+        print(r->left, space);
+    }
+    void printPreOrder(TreeNode *r){
+        if(r==NULL){
+            return;
+        }
+        cout<<r->value<<" ";
+        printPreOrder(r->left);
+        printPreOrder(r->right);
+    }
+};
+
+int main()
+{
+    int choice,val;
+    BST tree1;
+
+    do{
+          cout<<"Enter your choice : "<<endl;
+          cout<<"1. Insert Node "<<endl;
+          cout<<"2. Search Node "<<endl;
+          cout<<"3. Delete Node "<<endl;
+          cout<<"4. Print BST values "<<endl;
+          cout<<"5. In-Order Traversal "<<endl;
+          cout<<"6. Clear Screen "<<endl;
+          cout<<"0. Exit Program "<<endl;
+          cin>>choice;
+          TreeNode *newNode = new TreeNode();
+
+          switch(choice){
+              case 0:
+                  break;
+              case 1:
+                cout<<"Insert"<<endl;
+                cout<<"Enter the value of Tree node to insert in BST : ";
+                cin>>val;
+                newNode->value = val;
+                tree1.insertNode(newNode);
+                break;
+              case 2:
+                cout<<"Search"<<endl;
+                break;
+              case 3:
+                cout<<"Delete"<<endl;
+                break;
+             case 4:
+                cout<<"BST Values"<<endl;
+                tree1.print(tree1.root,5);
+                tree1.printPreOrder(tree1.root);
+                break;
+             case 5:
+                cout<<"In-Order Traversal "<<endl;
+                tree1.printPreOrder(tree1.root);
+                break;
+             case 6:
+                system("cls");
+                break;
+            default :
+                cout<<"Please enter valid choice number!"<<endl;
+                break;
+
+          }
+
+    }while(choice!=0);
+
+    return 0;
+}
